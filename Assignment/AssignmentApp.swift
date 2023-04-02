28783afebefa0e9602f58381a99d8139b2cded73
s5245510 <alexleung9407@gmail.com>
2023-04-02 15:36:09 +1000

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Tsz Hoi Leung on 2/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
